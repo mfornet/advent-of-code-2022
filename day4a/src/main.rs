5e73359c88b04d8b92880769ddf53c3d10c9e201
Marcelo Fornet <marcelo@aurora.dev>
2022-12-14 17:40:42 +0100

    day4: Add both parts solution

---
+++ day4a/src/main.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+
+fn main() {
+    let sol = aoc::input_lines(file!())
+        .map(|pair| {
+            let mut it = pair.split(",").map(|s| {
+                if let [a, b] = s
+                    .split("-")
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect_vec()[..]
+                {
+                    (a, b)
+                } else {
+                    panic!("bad input")
+                }
+            });
+            let a = it.next().unwrap();
+            let b = it.next().unwrap();
+
+            (a.0 >= b.0 && a.1 <= b.1 || b.0 >= a.0 && b.1 <= a.1) as u32
+        })
+        .sum::<u32>();
+
+    println!("{}", sol);
+}
