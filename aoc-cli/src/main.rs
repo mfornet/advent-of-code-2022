ea1e312ebc07d1a8cfcc2f8fc058b8669e4e3300
Marcelo Fornet <marcelo@aurora.dev>
2022-12-14 18:49:56 +0100

    feat: Download test case as well

--- aoc-cli/src/main.rs
+++ aoc-cli/src/main.rs
@@ -51,6 +51,42 @@ fn download_input(year: u32, day: u32) -> Result<String, String> {
     Ok(resp.text().unwrap())
 }
 
+fn extract_test(page_source: String) -> Option<String> {
+    let pat_test = regex::Regex::new(r#"<pre><code>([\s\S]*?)</code></pre>"#).unwrap();
+
+    let mut test_stream = pat_test
+        .captures_iter(&page_source)
+        .map(|cap| cap.get(1).unwrap().as_str().to_string())
+        .map(|data| html_escape::decode_html_entities(&data).to_string());
+
+    test_stream.next()
+}
+
+fn download_test(year: u32, day: u32) -> Option<String> {
+    // Create folder .aoc_cache if it does not exist
+    let cache_dir = project_dir().join(".aoc_cache");
+    if !cache_dir.exists() {
+        std::fs::create_dir(&cache_dir).unwrap();
+    }
+
+    // Try to download the page source from the cache first
+    let cache_file = cache_dir.join(format!("{}.html", day));
+    let page_source = if cache_file.exists() {
+        std::fs::read_to_string(&cache_file).unwrap()
+    } else {
+        let url = format!("https://adventofcode.com/{}/day/{}", year, day);
+        let resp = reqwest::blocking::Client::new().get(&url).send().unwrap();
+        assert!(resp.status().is_success());
+
+        // Save the page source to the cache
+        let page_source = resp.text().unwrap();
+        std::fs::write(&cache_file, &page_source).unwrap();
+        page_source
+    };
+
+    extract_test(page_source)
+}
+
 fn main() {
     dotenv::dotenv().ok();
     let args = Args::parse();
@@ -97,11 +133,56 @@ fn main() {
 
             // Download the input for the problem if it is a problem a
             if name.ends_with('a') {
-                match download_input(YEAR, name[3..name.len() - 1].parse().unwrap()) {
+                let day = name[3..name.len() - 1].parse().unwrap();
+                match download_input(YEAR, day) {
                     Ok(input) => std::fs::write(path.join("input.txt"), input).unwrap(),
                     Err(err) => println!("{}", err),
                 }
+
+                match download_test(YEAR, day) {
+                    Some(test) => std::fs::write(path.join("test.txt"), test).unwrap(),
+                    None => println!("Test not available"),
+                }
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn load_test(day: u32) -> String {
+        let file = project_dir().join(format!("day{}a", day)).join("test.txt");
+        std::fs::read_to_string(file)
+            .unwrap()
+            .trim_matches('\n')
+            .to_string()
+    }
+
+    fn load_all_tests() -> Vec<(u32, String)> {
+        (1..=25)
+            .filter_map(|day| {
+                project_dir()
+                    .join(format!("day{}a", day))
+                    .exists()
+                    .then(|| {
+                        dbg!(format!("Loading {}", day));
+                        (day, load_test(day))
+                    })
+            })
+            .collect()
+    }
+
+    #[test]
+    fn test_download_test() {
+        for (day, test_in) in load_all_tests() {
+            dbg!(format!("Checking test {}", day));
+            let parsed_test_in = download_test(2022, day);
+            assert_eq!(
+                parsed_test_in.unwrap().trim_matches('\n').to_string(),
+                test_in
+            );
         }
     }
 }
